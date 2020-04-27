e594598a70982c4c3b4e9230d37a175d824e27bf
streambinder <posta@davidepucci.it>
2020-04-27 14:37:44 +0200

    src: setup: first dump

---
+++ src/setup.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='Chopper',
+    version='1',
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src'),
+    entry_points={
+        'console_scripts': [
+            'chopper = chopper.__main__:main'
+        ]
+    },
+    author='Davide Pucci',
+    author_email='chopper@davidepucci.it',
+    description='Share all-sizes files using reconstructable manifests',
+    keywords='storage file-sharing encryption',
+    url='https://davidepucci.it/doc/chopper',
+    license='GPL-3.0',
+    project_urls={
+        'Documentation': 'https://davidepucci.it/doc/chopper',
+        'Source Code': 'https://github.com/streambinder/chopper',
+        'Bug Tracker': 'https://github.com/streambinder/chopper/issues',
+    },
+    classifiers=[
+        'License :: OSI Approved :: GPL-3.0'
+    ]
+
+)
